cbc8d96d24f1f04797ff17e0183d242ef5949ba9
Christian Schlack <christian@backhub.co>
2020-08-25 15:41:08 +0200

    bugfix: raise Exception for unknow errors

--- ext/errors.c
+++ ext/errors.c
@@ -31,10 +31,10 @@ void raise_exception(int error) {
           rb_raise(rb_eGitNetworkError, "%s", g_error->message ? g_error->message : "Network Error");
         } else if (g_error->klass == GIT_ERROR_HTTP) {
           rb_raise(rb_eGitHTTPError, "%s", g_error->message ? g_error->message : "HTTP Error");
+        } else {
+          rb_raise(rb_eGitError, "%s (%d)", (g_error && g_error->message) ? g_error->message : "Unkown Error",
+            (g_error && g_error->klass) ? g_error->klass : error);
         }
-      } else {
-        rb_raise(rb_eGitError, "%s (%d)", (g_error && g_error->message) ? g_error->message : "Unkown Error",
-          (g_error && g_error->klass) ? g_error->klass : error);
       }
   }
 }

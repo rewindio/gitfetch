99948b42c4e4b4b05d7096a62c64963e25d36275
Christian Schlack <christian@backhub.co>
2020-12-09 10:13:57 +0100

    Refactor exception handling

--- ext/errors.c
+++ ext/errors.c
@@ -1,45 +1,62 @@
 #include <string.h>
 #include "gitfetch.h"
 
+#define OR(a, b) a ? a : b
+#define raise(e, m) rb_raise(e, "%s", OR(g_error->message, m))
+
 void raise_exception(int error) {
-  const git_error *g_error = git_error_last();
+  const git_error *g_error;
 
-  switch (error) {
-    case GIT_EAUTH:
-      rb_raise(rb_eGitAuthenticationError, "%s", (g_error && g_error->message) ? g_error->message : "Authentication Error");
-      break;
+  if (error == GIT_OK) { return; }
+  g_error = git_error_last();
 
-    case GIT_EEOF:
-      rb_raise(rb_eGitEofError, "%s", (g_error && g_error->message) ? g_error->message : "Unexpected EOF");
-      break;
+  if (g_error) {
+    switch (error) {
+      case GIT_EAUTH:
+        raise(rb_eGitAuthenticationError, "Authentication Error");
+        break;
 
-    case GIT_EEXISTS:
-      rb_raise(rb_eGitExistsError, "%s", (g_error && g_error->message) ? g_error->message : "Object exists");
-      break;
+      case GIT_EEOF:
+        raise(rb_eGitEofError, "Unexpected EOF");
+        break;
 
-    case GIT_ENOTFOUND:
-      rb_raise(rb_eGitNotFoundError, "%s", (g_error && g_error->message) ? g_error->message : "Object not found");
-      break;
+      case GIT_EEXISTS:
+        raise(rb_eGitExistsError, "Object exists");
+        break;
 
-    case GIT_EUSER:
-      rb_raise(rb_eGitUserError, "%s", (g_error && g_error->message) ? g_error->message : "User defined error");
+      case GIT_ENOTFOUND:
+        raise(rb_eGitNotFoundError, "Object not found");
+        break;
 
-    default:
-      if (g_error) {
-        if (g_error->klass == GIT_ERROR_NONE) {
-          return;
-        } else if (g_error->message && strstr(g_error->message, "repository not exported")) {
-          rb_raise(rb_eGitRepositoryNotExportedError, "%s", g_error->message);
-        } else if (g_error->klass == GIT_ERROR_HTTP) {
-          rb_raise(rb_eGitHTTPError, "%s", g_error->message ? g_error->message : "HTTP Error");
-        } else if (g_error->klass == GIT_ERROR_NET) {
-          rb_raise(rb_eGitNetworkError, "%s", g_error->message ? g_error->message : "Network Error");
-        } else if (g_error->klass == GIT_ERROR_SSL) {
-          rb_raise(rb_eGitSSLError, "%s", g_error->message ? g_error->message : "SSL Error");
-        } else {
-          rb_raise(rb_eGitError, "%s (%d)", g_error->message ? g_error->message : "Unkown Error",
-            g_error->klass ? g_error->klass : error);
+      case GIT_EUSER:
+        raise(rb_eGitUserError, "User defined error");
+        break;
+
+      default:
+        switch (g_error->klass) {
+          case GIT_ERROR_HTTP:
+            raise(rb_eGitHTTPError, "HTTP Error");
+            break;
+
+          case GIT_ERROR_NET:
+            if (strstr(g_error->message, "repository not exported")) {
+              raise(rb_eGitRepositoryNotExportedError, NULL);
+            } else {
+              raise(rb_eGitNetworkError, "Network Error");
+            }
+            break;
+
+          case GIT_ERROR_SSL:
+            raise(rb_eGitSSLError, "SSL Error");
+            break;
+
+          default:
+            rb_raise(rb_eGitError, "%s (%d)", OR(g_error->message, "Unknown Error"), g_error->klass);
+            break;
         }
+        break;
       }
-  }
+    } else {
+      rb_raise(rb_eGitError, "Unkown Error (%d)", error);
+    }
 }
